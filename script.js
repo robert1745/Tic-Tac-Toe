0a42515f859ac8b00c07861ebd90913219425c8f
robert1745 <0745naveen@gmail.com>
2025-07-19 18:25:35 +0000

    added game logic

--- script.js
+++ script.js
@@ -6,15 +6,46 @@ document.addEventListener("DOMContentLoaded", () => {
   let currPlayer = "X";
   let board = ["", "", "", "", "", "", "", "", ""];
   let gameactive = true;
+  const winningCombos = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
 
+    [0, 3, 6], // left column
+    [1, 4, 7], // middle column
+    [2, 5, 8], // right column
+    
+    [0, 4, 8], // diagonal
+    [2, 4, 6], // other diagonal
+  ];
   cells.forEach((cell) => {
     cell.addEventListener("click", (e) => {
-      if (cell.textContent !== "") {
+      const index = cell.getAttribute("data-index");
+
+      if (cell.textContent !== "" || !gameactive) {
         return;
       }
-      cell.textContent = currPlayer; 
+      cell.textContent = currPlayer;
+      board[index] = currPlayer;
+      if (checkWinner()) {
+        message.textContent = `Player ${currPlayer} wins!`;
+        gameactive = false;
+        return;
+      }
+      if (!board.includes("")) {
+        message.textContent = `Its a draw!`;
+        gameactive = false;
+        return;
+      }
       message.textContent = `Player ${currPlayer === "X" ? "O" : "X"}'s turn`;
       currPlayer = currPlayer === "X" ? "O" : "X";
     });
   });
+
+  function checkWinner() {
+    return winningCombos.some((combo) => {
+      const [a, b, c] = combo;
+      return board[a] && board[a] === board[b] && board[a] === board[c];
+    });
+  }
 });
